34cb605a172502400891c34b5adcf8a9683c6f76
chetan9518 <amanjangraofficial@gmail.com>
2026-07-11 22:36:35 +0530

    Add new allowed origin for CORS

--- backend/index.ts
+++ backend/index.ts
@@ -26,7 +26,8 @@ let app = express();
 app.use(express.json());
 const allowedOrigins = [
   "http://localhost:5173", 
-  "https://skillmatch.sbs"
+  "https://skillmatch.sbs",
+  "https://skillmatchbackeddatabase.pages.dev"
 ];
 app.use(cors({
   origin: allowedOrigins,
